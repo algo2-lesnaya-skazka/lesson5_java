2c4496b7233b1c40f695653ba4d12f8a12241113
YarikBespalov <yarikbespalov@mail.ru>
2019-11-13 20:02:09 +0300

    Add lesson5_java files

---
+++ HElloworld/src/Snippet.java
@@ -0,0 +1,8 @@
+public class Snippet {
+ public static void main(String agrs[]) {
+	 int[] mytir = {1, 3, 5, 7, 9, 11, 13, 15, 19, 21};
+	 for(int i = 0; i < 10; i++){
+		    System.out.println(mytir[i]);
+		}
+}
+}
