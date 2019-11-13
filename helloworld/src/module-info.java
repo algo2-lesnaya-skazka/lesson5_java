2e1d7634ae71c260617b9fbb51b0335c3800c09f
Michail Chernega <kakas.83@mail.ru>
2019-11-13 18:56:59 +0300

    Add lesson5_java files

---
+++ helloworld/src/module-info.java
@@ -0,0 +1,18 @@
+public class helloworld { 
+
+	public static void main(String[] args) { 
+
+String a = "* * ***** *   *    ***      "; 
+String b = "* * *     *   *   *   * "; 
+String c = "*** ***** *   *   *   *   "; 
+String d = "* * *     *   *   *   *    "; 
+String e = "* * ***** *** ***  ***   "; 
+
+System.out.println(a); 
+System.out.println(b); 
+System.out.println(c); 
+System.out.println(d); 
+System.out.println(e); 
+
+} 
+}
